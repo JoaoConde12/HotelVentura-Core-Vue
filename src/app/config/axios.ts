47d902fe5f3ba69ed06a57f7a7b2fece67b78f87
Joao12 <132232545+JoaoConde12@users.noreply.github.com.>
2026-01-22 18:24:11 -0500

    Actualizado el axios

--- src/app/config/axios.ts
+++ src/app/config/axios.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
 
 api.interceptors.request.use((config) => {
